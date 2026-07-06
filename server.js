aee9839d2c7d09f8d900c2cf2e9197c6bd2dfe61
Dipak Nagdev <nagdevdipak53@gmail.com>
2026-07-06 21:42:59 +0530

    server js file

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ require("dotenv").config();
 
 const app = express();
 app.use(cors());
+
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 app.use('/Uploads',express.static('Uploads'))
@@ -26,4 +27,7 @@ app.use((req, res, next) => {
   console.log(req.method, req.url);
   next();
 });
-app.listen(5000, () => console.log("Server running on port 5000"));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT,"0.0.0.0", () => console.log(`Server running on port ${PORT}`));

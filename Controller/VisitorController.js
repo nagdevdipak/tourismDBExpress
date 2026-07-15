25f077d964df947e339ce7d937ab0e1ccb1a85ba
Dipak Nagdev <nagdevdipak53@gmail.com>
2026-07-15 20:49:07 +0530

    code change

--- Controller/VisitorController.js
+++ Controller/VisitorController.js
@@ -11,9 +11,8 @@ function generateOTP() {
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
-  requireTLS: true,
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
